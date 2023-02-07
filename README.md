c69ea8c9d667c33e4010e3fecf389570a8ccfb17
Jesus Lara <jelara99@gmail.com>
2023-02-07 15:41:57 -0600

    modulo 1 funcional

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CediApi
-API para trabajo en cedi app
+## Descripcion del proyecto
+
+1. Este repositorio es parte de un proyecto laboral para la unidad de centro de soporte de Fundación Omar Dengo en la subarea de almacenamiento y distribución. 
+2. El codigo corresponde a una estructura de backend especificamente una API desarrollada para solicitudes especificas y no puede responder a otro tipo de consultas fuera de los requerimientos del frontend [Cediapp](https://github.com/GsusLara/cediapp)
+---
+## Usos de la API
+recepción = recibe unos Archivos de Excel especificos y carga la informacion necesaria en la BD
+
+---
+
+## Composición del proyecto
+
+* flask 
+* pandas 
+* openpyxl 
+* xlrd 
+* mysql-connector-python 
--- app.py
+++ app.py
@@ -20,21 +20,31 @@ def page():
 def upload():
     files = request.files.getlist('file')
     if files:#si existen archivos
-        info = []#Inicializa una lista vacía para guardar la información
+        reponse = []
         for i in files:#Itera la lista de archivos xls
-            df = pd.read_excel(i, header=25)#Crea un dataframe a partir del xls y asigna la fila 25 como encabezado 
-            df = df.iloc[:-2]#elimina las ultimas dos lineas del dataframe ya que no son informacion útil
-            df['Placa'] = df['Placa'].astype(int)#convierte la columna placa en datos enteros
-            descripcion = list(df['Descripción'])#crea una lista con la descripción
-            placa = list(df['Placa'])#crea una lista con los numeros de placa
-            serie = list(df['Número Serie'])#crea una lista con los numeros de serie
-            salida = list(zip(descripcion, placa, serie))#unifica los datos en una lista de tuplas
-            info.append(salida)# Agrega los datos del archivo excel a la nueva lista de salida
-            accion = str(i.filename)#toma el nombre del archivo 
-            equipos = (len(df))#toma la cantidad de equipos
-            consulta.execute(f'INSERT INTO Acciones (idAccion, FechaEntrada, FechaSalida, CantidadEquipos, IdEstadoAccion, IdAsignaciones) VALUES (\'{accion}\', NOW(), NULL, {equipos}, 1, 3);')
-            mydb.commit()
-        return ('todo bien') # Devuelve la lista de dalida con la información util de todos los archivos Excel cargados
+    #bloque codigo para cargar las datos de la acción en la tabla de la bd
+            AccionCompleta = pd.read_excel(i)
+            numeroAccion = AccionCompleta.iloc[4, 0].split()[-1]
+            consulta.execute(f'SELECT COUNT(*) FROM Acciones WHERE IdAccion = \'{numeroAccion}\'')
+            respuestaBd = consulta.fetchone()[0]
+            if respuestaBd == 0:
+                df = pd.read_excel(i, header=25)#Crea un dataframe a partir del xls y asigna la fila 25 como encabezado 
+                df = df.iloc[:-2]#elimina las ultimas dos lineas del dataframe ya que no son informacion útil
+                cantidadEquipos = (len(df))#toma la cantidad de equipos
+                consulta.execute(f'INSERT INTO Acciones (IdAccion,  CantidadEquipos) VALUES (\'{numeroAccion}\', {cantidadEquipos});')
+                mydb.commit()
+    #bloque de codigo para cargar los equipos en la tabla de la bd
+                df['Placa'] = df['Placa'].astype(int)#convierte la columna placa en datos enteros
+                descripcion = list(df['Descripción'])#crea una lista con la descripción
+                placa = list(df['Placa'])#crea una lista con los numeros de placa
+                serie = list(df['Número Serie'])#crea una lista con los numeros de serie
+                equipos = list(zip(descripcion, placa, serie))#unifica los datos en una lista de tuplas
+                reponse.append(f"Se cargó como {numeroAccion}")
+            else:
+                reponse.append("ya esta en bd")
+                
+            
+        return (reponse)
     else:
         return jsonify({"error": "No hay archivos para procesar."})# Si no hay archivos para procesar, devuelve un error
 
