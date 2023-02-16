40d217e997f5e3f5978c6632eec364b83414454c
Jesus Lara <jelara99@gmail.com>
2023-02-16 15:18:13 -0600

    funcionalidad modulo1 probada

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_mysqldb import MySQL
 import pandas as pd
-import mysql.connector
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'PassworD'
+app.config['MYSQL_DB'] = 'cediSoporte'
+app.config['MYSQL_HOST'] = '10.14.11.71'
+
+mysql = MySQL(app)
 
 @app.route('/', methods = ['GET'])
 def page():
@@ -15,9 +22,7 @@ def upload():
     #Recibe y analiza si existen archivos
     files = request.files.getlist('file')
     if files:
-    #inicia la conexion con la BD
-        mydb = mysql.connector.connect(host="10.14.11.71",user="root",password="PassworD",database="cediSoporte")
-        consulta = mydb.cursor()
+        consulta = mysql.connection.cursor()
         response = []
         for i in files:#Itera la lista de archivos xls
     #bloque codigo para cargar las datos de la acción en la tabla de la bd
@@ -39,9 +44,8 @@ def upload():
                 for computadora in equipos:
                     consulta.execute(f'INSERT INTO Equipos (Placa, Serie, Descripcion, IdAccion) VALUES ({computadora[1]},"{computadora[2]}", "{computadora[0]}","{numeroAccion}");')
                 response.append(numeroAccion)
-        mydb.commit()
+        mysql.connection.commit()
         consulta.close()
-        mydb.close()
         if len(response) != 0:
             response.append("Datos cargados Exitosamente")
             return jsonify(response),200
@@ -52,26 +56,35 @@ def upload():
 
 @app.route('/acciones', methods=['GET'])
 def infoAcciones():
-    mydb = mysql.connector.connect(host="10.14.11.71",user="root",password="PassworD",database="cediSoporte")
-    consulta = mydb.cursor()
-    consulta.execute("SELECT * FROM Acciones")
-    datos = consulta.fetchall()
-    columnas = [columna[0] for columna in consulta.description]
+    consultaAcciones = mysql.connection.cursor()
+    consultaAcciones.execute("SELECT * FROM Acciones")
+    datos = consultaAcciones.fetchall()
+    columnas = [columna[0] for columna in consultaAcciones.description]
     lista_de_diccionarios = [dict(zip(columnas, fila)) for fila in datos]
-    consulta.close()
-    mydb.close()
+    consultaAcciones.close()
     return jsonify(lista_de_diccionarios),200
 
 @app.route('/equipos/<path:numeroAccion>', methods=['GET'])
 def infoEquipos(numeroAccion):
-    mydbEquipos = mysql.connector.connect(host="10.14.11.71",user="root",password="PassworD",database="cediSoporte")
-    consultaEquipos = mydbEquipos.cursor()
+    consultaEquipos = mysql.connection.cursor()
     consultaEquipos.execute(f"SELECT * FROM Equipos WHERE IdAccion = '{numeroAccion}'")
     columnas = [columna[0] for columna in consultaEquipos.description]
     listaEquipos = [dict(zip(columnas, fila)) for fila in consultaEquipos.fetchall()]
     consultaEquipos.close()
-    mydbEquipos.close()
     return jsonify(listaEquipos),200
 
+@app.route('/estadoAccion', methods=['PUT'])
+def cambioEstadoAccion():
+    data = request.get_json()
+    numeroAccion = data['id_accion']
+    nuevoEstado = data['id_estado']
+    if nuevoEstado >=1 and nuevoEstado <= 5:
+        consultaEstadoAccion = mysql.connection.cursor()
+        consultaEstadoAccion.execute("UPDATE Acciones SET IdEstadoAccion = %s WHERE IdAccion = %s", (nuevoEstado, numeroAccion))
+        mysql.connection.commit()
+        consultaEstadoAccion.close()
+        return jsonify({"messaje": "se actualizó la acción correctamente"}),200
+    else:
+        return jsonify({"error": "inesperado"}),400
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
